9165b671b0093e7bc7b4f4ac3a73b36ad84588f6
Taeho Kim <jyte82@gmail.com>
2018-01-09 01:34:27 +0900

    Use AutoValue to generate Child*Event classes

--- firebase-database/src/main/java/com/androidhuman/rxfirebase2/database/ChildAddEvent.java
+++ firebase-database/src/main/java/com/androidhuman/rxfirebase2/database/ChildAddEvent.java
@@ -1,62 +1,21 @@
 package com.androidhuman.rxfirebase2.database;
 
+import com.google.auto.value.AutoValue;
 import com.google.firebase.database.DataSnapshot;
 
 import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-public final class ChildAddEvent extends ChildEvent {
-
-    private final String previousChildName;
+@AutoValue
+public abstract class ChildAddEvent extends ChildEvent {
 
     @CheckResult
     @NonNull
     public static ChildAddEvent create(DataSnapshot dataSnapshot, String previousChildName) {
-        return new ChildAddEvent(dataSnapshot, previousChildName);
-    }
-
-    private ChildAddEvent(DataSnapshot dataSnapshot, String previousChildName) {
-        super(dataSnapshot);
-        this.previousChildName = previousChildName;
+        return new AutoValue_ChildAddEvent(dataSnapshot, previousChildName);
     }
 
     @Nullable
-    public String previousChildName() {
-        return previousChildName;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        if (!super.equals(o)) {
-            return false;
-        }
-
-        ChildAddEvent that = (ChildAddEvent) o;
-
-        return previousChildName != null ? previousChildName.equals(that.previousChildName)
-                : that.previousChildName == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + dataSnapshot.hashCode();
-        result = 31 * result + (previousChildName != null ? previousChildName.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "ChildAddEvent{"
-                + "dataSnapshot=" + dataSnapshot
-                + ", previousChildName='" + previousChildName + '\''
-                + '}';
-    }
+    public abstract String previousChildName();
 }
--- firebase-database/src/main/java/com/androidhuman/rxfirebase2/database/ChildEvent.java
+++ firebase-database/src/main/java/com/androidhuman/rxfirebase2/database/ChildEvent.java
@@ -6,34 +6,10 @@ import android.support.annotation.NonNull;
 
 public abstract class ChildEvent {
 
-    final DataSnapshot dataSnapshot;
+    ChildEvent() {
 
-    ChildEvent(DataSnapshot dataSnapshot) {
-        this.dataSnapshot = dataSnapshot;
     }
 
     @NonNull
-    public DataSnapshot dataSnapshot() {
-        return dataSnapshot;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        ChildEvent that = (ChildEvent) o;
-
-        return dataSnapshot.equals(that.dataSnapshot);
-
-    }
-
-    @Override
-    public int hashCode() {
-        return dataSnapshot.hashCode();
-    }
+    public abstract DataSnapshot dataSnapshot();
 }
