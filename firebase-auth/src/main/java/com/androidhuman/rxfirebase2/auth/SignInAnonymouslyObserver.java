f2adeca511c6d6898e20ba329a79d399b94e5635
Grzegorz Miszewski <gmiszewski@getresponse.com>
2018-11-06 12:26:51 +0100

    Added methods to return AuthResult on signIn instead of FirebaseUser

--- firebase-auth/src/main/java/com/androidhuman/rxfirebase2/auth/SignInAnonymouslyObserver.java
+++ firebase-auth/src/main/java/com/androidhuman/rxfirebase2/auth/SignInAnonymouslyObserver.java
@@ -1,18 +1,12 @@
 package com.androidhuman.rxfirebase2.auth;
 
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import com.androidhuman.rxfirebase2.core.OnCompleteDisposable;
-
-import android.support.annotation.NonNull;
 
 import io.reactivex.Single;
 import io.reactivex.SingleObserver;
 
-final class SignInAnonymouslyObserver extends Single<FirebaseUser> {
+final class SignInAnonymouslyObserver extends Single<AuthResult> {
 
     private final FirebaseAuth instance;
 
@@ -21,31 +15,12 @@ final class SignInAnonymouslyObserver extends Single<FirebaseUser> {
     }
 
     @Override
-    protected void subscribeActual(SingleObserver<? super FirebaseUser> observer) {
-        Listener listener = new Listener(observer);
+    protected void subscribeActual(SingleObserver<? super AuthResult> observer) {
+        AuthResultListener listener = new AuthResultListener(observer);
         observer.onSubscribe(listener);
 
         instance.signInAnonymously()
                 .addOnCompleteListener(listener);
     }
 
-    static final class Listener extends OnCompleteDisposable<AuthResult> {
-
-        private final SingleObserver<? super FirebaseUser> observer;
-
-        Listener(@NonNull SingleObserver<? super FirebaseUser> observer) {
-            this.observer = observer;
-        }
-
-        @Override
-        public void onComplete(@NonNull Task<AuthResult> task) {
-            if (!isDisposed()) {
-                if (!task.isSuccessful()) {
-                    observer.onError(task.getException());
-                } else {
-                    observer.onSuccess(task.getResult().getUser());
-                }
-            }
-        }
-    }
 }
--- firebase-auth/src/main/java/com/androidhuman/rxfirebase2/auth/SignInWithCredentialObserver.java
+++ firebase-auth/src/main/java/com/androidhuman/rxfirebase2/auth/SignInWithCredentialObserver.java
@@ -1,19 +1,13 @@
 package com.androidhuman.rxfirebase2.auth;
 
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import com.androidhuman.rxfirebase2.core.OnCompleteDisposable;
-
-import android.support.annotation.NonNull;
 
 import io.reactivex.Single;
 import io.reactivex.SingleObserver;
 
-final class SignInWithCredentialObserver extends Single<FirebaseUser> {
+final class SignInWithCredentialObserver extends Single<AuthResult> {
 
     private final FirebaseAuth instance;
 
@@ -25,31 +19,12 @@ final class SignInWithCredentialObserver extends Single<FirebaseUser> {
     }
 
     @Override
-    protected void subscribeActual(SingleObserver<? super FirebaseUser> observer) {
-        Listener listener = new Listener(observer);
+    protected void subscribeActual(SingleObserver<? super AuthResult> observer) {
+        AuthResultListener listener = new AuthResultListener(observer);
         observer.onSubscribe(listener);
 
         instance.signInWithCredential(credential)
                 .addOnCompleteListener(listener);
     }
 
-    static final class Listener extends OnCompleteDisposable<AuthResult> {
-
-        private final SingleObserver<? super FirebaseUser> observer;
-
-        Listener(@NonNull SingleObserver<? super FirebaseUser> observer) {
-            this.observer = observer;
-        }
-
-        @Override
-        public void onComplete(@NonNull Task<AuthResult> task) {
-            if (!isDisposed()) {
-                if (!task.isSuccessful()) {
-                    observer.onError(task.getException());
-                } else {
-                    observer.onSuccess(task.getResult().getUser());
-                }
-            }
-        }
-    }
 }
